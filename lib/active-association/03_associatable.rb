13b9865b904c009d26e4e4f96e204ffdffa2d602
CrookedKings <John.Mahowald@gmail.com>
2014-10-08 16:23:25 -0700

    revised README

--- lib/active-association/03_associatable.rb
+++ lib/active-association/03_associatable.rb
@@ -1,7 +1,6 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
 
-# Phase IVa
 class AssocOptions
   attr_accessor(
     :foreign_key,
@@ -47,7 +46,6 @@ class HasManyOptions < AssocOptions
 end
 
 module Associatable
-  # Phase IVb
   def belongs_to(name, options = {})
     self.assoc_options[name] = BelongsToOptions.new(name, options)
     
